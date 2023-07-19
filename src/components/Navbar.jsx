31c154c5916af50b70e7d6625e45324cdef640fa
hosseinahmadizadeh <hosseinahmadizadeh1377@gmail.com>
2023-07-19 15:18:54 +0330

    install react-icons and adding font & icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import {Navbar as NavbarBs, Button} from "react-bootstrap"
+import {BsCart} from "react-icons/bs"
+
+
 
 function Navbar (){
     return (
         <NavbarBs className="border-bottom border-secondary">
             <NavbarBs.Collapse className="justify-content-end">
-                <Button variant="btn btn-outline-secondary" className="text-white">Orders</Button>
+                <Button variant="btn btn-outline-secondary" className="text-white" >
+                    <BsCart className="mx-2 mb-1"></BsCart>Orders</Button>
             </NavbarBs.Collapse>
         </NavbarBs>
     )
