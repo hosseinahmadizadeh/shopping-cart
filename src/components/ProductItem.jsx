29a5e6c491b099dbb74eb70f0c50931a382b6e7a
hosseinahmadizadeh <hosseinahmadizadeh1377@gmail.com>
2023-07-24 16:08:20 +0330

    delete item from cart

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 
 import { Card, Button, Form, Row, Col } from "react-bootstrap";
+import { BsFillTrash3Fill } from "react-icons/bs";
 
 import { CartContext } from "../context/CartContext";
 
@@ -26,25 +27,33 @@ function ProductItem({ product }) {
         {productQuantity > 0 ? (
           <>
             <Form as={Row}>
-              <Form.Label column="true" sm="6" className="text-white">
+              <Form.Label column="true" sm="12" className="text-white">
                 Count: {productQuantity}
               </Form.Label>
-              <Col sm="6">
+              <Col sm="12">
                 <Button
                   onClick={() => cart.addItemToCart(product.id)}
-                  sm="6"
-                  className="mx-2 text-white"
+                  sm="12"
+                  className="mx-1 text-white"
                   variant="btn btn-outline-secondary"
                 >
                   +
                 </Button>
                 <Button
                   onClick={() => cart.removeItemFromCart(product.id)}
-                  sm="6"
-                  className="mx-2 text-white"
+                  sm="12"
+                  className="mx-1 text-white"
                   variant="btn btn-outline-secondary"
                 >
                   -
+                </Button>
+                <Button
+                  onClick={() => cart.deleteFromCart(product.id)}
+                  sm="12"
+                  className="mx-1"
+                  variant="btn btn-outline-secondary"
+                >
+                  <BsFillTrash3Fill className="mb-1"></BsFillTrash3Fill>
                 </Button>
               </Col>
             </Form>
